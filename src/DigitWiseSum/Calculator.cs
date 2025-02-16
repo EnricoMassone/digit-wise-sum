5ae05553df3284020e76f2848934e8efdfc7c59d
Enrico Massone <enrico.massone@hotmail.it>
2025-02-16 21:55:29 +0100

    Implemented digit wise sum

---
+++ src/DigitWiseSum/Calculator.cs
@@ -0,0 +1,67 @@
+﻿namespace DigitWiseSum;
+
+/// <summary>
+/// This class contains the method to implement the digit wise sum
+/// </summary>
+public static class Calculator
+{
+  /// <summary>
+  /// Computes the digit wise sum between an array of base 10 digits and a non negative integer number.
+  /// </summary>
+  /// <param name="left">An array of base 10 digits</param>
+  /// <param name="right">A non negative integer number</param>
+  /// <returns>
+  /// An array of base 10 digits representing the sum of the provided parameters.
+  /// </returns>
+  public static uint[] Sum(uint[] left, uint right)
+  {
+    ArgumentNullException.ThrowIfNull(left);
+
+    // Create a stack to write the digits composing the result.
+    // Digits will be pushed from the least significative to the most significative.
+    var digits = new Stack<uint>();
+
+    // What remains to be added to left
+    var remainder = right;
+
+    // Iterate over the digits of left, from the least significative to the most significative
+    for (int i = left.Length - 1; i >= 0; i--)
+    {
+      // take the i-th item inside the array
+      var current = left[i];
+
+      // compute the sum of i-th array item and remainder
+      var sum = remainder + current;
+
+      // compute the digit to be pushed to the stack
+      var digit = sum % 10;
+      digits.Push(digit);
+
+      // compute the remainder still to be added
+      remainder = sum / 10;
+    }
+
+    if (remainder > 0)
+    {
+      var digitsOfRemainder = remainder.ToDigitsArray();
+
+      // digits of remainder must be pushed to the stack, from the least significative to the most significative
+      for (int i = digitsOfRemainder.Length - 1; i >= 0; i--)
+      {
+        var current = digitsOfRemainder[i];
+        digits.Push(current);
+      }
+    }
+
+    // create the result array
+    var result = new uint[digits.Count];
+
+    // pop from the stack and fill the result array from the most significative digit to the least significative digit
+    for (int i = 0; i < result.Length; i++)
+    {
+      result[i] = digits.Pop();
+    }
+
+    return result;
+  }
+}
---
+++ test/DigitWiseSum.UnitTests/CalculatorTests.cs
@@ -0,0 +1,140 @@
+﻿using FluentAssertions;
+using static DigitWiseSum.Calculator;
+
+namespace DigitWiseSum.UnitTests;
+
+public sealed class CalculatorTests
+{
+  [Fact]
+  public void Sum_Throws_ArgumentNullException_When_Left_Is_Null()
+  {
+    // ARRANGE
+    const uint right = 5;
+
+    // ACT
+    var act = () => Sum(null!, right);
+
+    // ASSERT
+    act.Should().ThrowExactly<ArgumentNullException>().WithParameterName("left");
+  }
+
+  [Fact]
+  public void Sum_Works_When_Right_Is_Zero()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 0;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([1, 2, 3]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_Left_Is_Zero()
+  {
+    // ARRANGE
+    var left = new uint[] { 0 };
+    const uint right = 6;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([6]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_Only_Least_Significative_Digit_Of_Left_Is_Changed()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 5;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([1, 2, 8]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_Last_Two_Digits_Of_Left_Are_Changed()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 8;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([1, 3, 1]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_All_Digits_Of_Left_Are_Changed()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 302;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([4, 2, 5]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_All_Digits_Of_Left_Are_Changed_And_There_Is_One_Digit_Remainder()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 926;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([1, 0, 4, 9]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_All_Digits_Of_Left_Are_Changed_And_There_Is_Two_Digits_Remainder()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 11233;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([1, 1, 3, 5, 6]);
+  }
+
+  [Fact]
+  public void Sum_Works_When_All_Digits_Of_Left_Are_Changed_And_There_Is_Three_Digits_Remainder()
+  {
+    // ARRANGE
+    var left = new uint[] { 1, 2, 3 };
+    const uint right = 230600;
+
+    // ACT
+    var result = Sum(left, right);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Should().Equal([2, 3, 0, 7, 2, 3]);
+  }
+}
